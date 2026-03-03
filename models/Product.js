b3a68e7a844413e0e7d603db38db437b790dcd55
Yeanpier <yeanpier03.11@gmail.com>
2026-03-02 21:01:46 -0500

    Actualización visual y mejoras

--- models/Product.js
+++ models/Product.js
@@ -1,16 +1,15 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: String,
-  price: { type: Number, required: true },
-  stock: { type: Number, required: true },
-  image: String,
-  category: {
-    type: String,
-    enum: ["Tecnología", "Electrodomésticos", "Línea Blanca"],
-    required: true
-  }
-});
+  nombre: { type: String, required: true },
+  descripcion: { type: String, default: "" },
+  precio: { type: Number, required: true },
+  existencias: { type: Number, default: 0 },
+  image: { type: String, default: "" },
+  brand: { type: String, default: "" },
+  category: { type: String, default: "" },
+  subcategory: { type: String, default: "" },
+  specs: { type: [String], default: [] }
+}, { timestamps: true });
 
 module.exports = mongoose.model("Product", productSchema);
